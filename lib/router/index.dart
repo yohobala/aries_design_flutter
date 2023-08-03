cf458c841d6ff84b90035f5fc97aedb52d777d41
1997926hh <762100246@qq.com>
2023-08-03 18:50:54 +0800

    更新

--- lib/router/index.dart
+++ lib/router/index.dart
@@ -1,6 +1,22 @@
+export 'widget.dart';
+
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:aries_design_flutter/aries_design_flutter.dart';
 import 'package:path/path.dart';
+
+Set<String> _routeNames = {};
+List<AriRouteItem> _routeItems = [];
+
+bool _isNameUnique(String name) {
+  if (_routeNames.contains(name)) {
+    return false;
+  } else {
+    _routeNames.add(name);
+    return true;
+  }
+}
 
 /// AriRoute单个路由的配置
 ///
@@ -13,7 +29,7 @@ import 'package:path/path.dart';
 ///
 /// *assert*
 /// - 如果[hasNavigation]为true，[navigationConfig]和[icon]不能为null,
-///
+/// - [name]必须唯一
 ///
 class AriRouteItem {
   AriRouteItem(
@@ -24,21 +40,27 @@ class AriRouteItem {
       this.icon,
       this.hasNavigation = false,
       this.navigationConfig,
-      this.children = const []})
+      this.children = const [],
+      this.isCover = false})
       : assert(
           !hasNavigation ||
-              (hasNavigation && navigationConfig != null && icon != null),
-        );
+              (hasNavigation &&
+                  navigationConfig != null &&
+                  icon != null &&
+                  widget != null),
+        ),
+        assert(isCover || _isNameUnique(name));
 
-  /// 路由名称，保证唯一性
+  /// 路由名称，保证唯一性,主要用于路由跳转
   final String name;
 
   /// 路由对应的widget
-  final Widget Function(BuildContext) widget;
+  /// 如果[hasNavigation]为true，该值无效
+  Widget Function(BuildContext)? widget;
 
   /// 路由的route
   /// 例如：/home
-  final String route;
+  String route;
 
   /// 路由的索引
   final int? index;
@@ -56,13 +78,45 @@ class AriRouteItem {
   /// 导航栏的配置
   final AriRouteItemNavigationConfig? navigationConfig;
 
+  /// 子路由
   final List<AriRouteItem> children;
+
+  /// 路由的标签, 用于底部导航栏
+  String label = "";
+
+  /// 当这个值为true时，如果[name]相同，会覆盖原来的路由
+  /// 默认为false
+  bool isCover;
+
+  AriRouteItem copyWith({
+    String? name,
+    Widget Function(BuildContext)? widget,
+    String? route,
+    int? index,
+    Icon? icon,
+    bool? hasNavigation,
+    AriRouteItemNavigationConfig? navigationConfig,
+    List<AriRouteItem>? children,
+    bool isCover = true,
+  }) {
+    return AriRouteItem(
+      name: name ?? this.name,
+      widget: widget ?? this.widget,
+      route: route ?? this.route,
+      index: index ?? this.index,
+      icon: icon ?? this.icon,
+      hasNavigation: hasNavigation ?? this.hasNavigation,
+      navigationConfig: navigationConfig ?? this.navigationConfig,
+      children: children ?? this.children,
+      isCover: isCover,
+    );
+  }
 }
 
 /// 底部导航路由的配置
 class AriRouteItemNavigationConfig {
   AriRouteItemNavigationConfig(
-      {this.initialRoute = "/", required this.navigationItemNames});
+      {this.initialRoute = "/", required this.navigationItems});
 
   /// 初始路由,默认为/，这个和[AriRouteItem]的[route]不一样，
   /// 即使[AriRouteItem]的[route]设置为/home，这里也可以设置为/user
@@ -72,7 +126,7 @@ class AriRouteItemNavigationConfig {
   final String initialRoute;
 
   /// 需要配置到底部导航栏的路由名字，是[AriRouteItem][children]中路由的name
-  final List<String> navigationItemNames;
+  final List<AriRouteItem> navigationItems;
 }
 
 class AriRouter {
@@ -83,21 +137,18 @@ class AriRouter {
   // 存储单例
   static final AriRouter _instance = AriRouter._internal();
 
-  //**************** 私有变量 ****************
-  late List<AriRouteItem> _routes = [];
-  Set<String> _routeNames = {};
-
   //**************** 公有方法 ****************
   // 得到路由表
   Map<String, Widget Function(BuildContext)> getRoutes() {
     final Map<String, Widget Function(BuildContext)> routeMap =
-        _generateRoutes(_routes);
+        _generateRoutes(_routeItems);
     return routeMap;
   }
 
   // 设置路由表
-  void setRoutes(List<AriRouteItem> routes) {
-    _insertRoutes(routes);
+  void setRoutes(List<AriRouteItem> routeItems,
+      [Widget Function(BuildContext)? bottomNavigationBar]) {
+    _insertRoutes(routeItems, "/", bottomNavigationBar);
   }
 
   void pushNamed(BuildContext context, String routeName) {
@@ -107,41 +158,117 @@ class AriRouter {
   //**************** 私有方法 ****************
   /// 生成路由表
   Map<String, Widget Function(BuildContext)> _generateRoutes(
-      List<AriRouteItem> routes) {
+      List<AriRouteItem> routeItems) {
     final Map<String, Widget Function(BuildContext)> routeMap = {};
-    routes.forEach((element) {
-      routeMap[element.route] = element.widget;
-    });
+    for (var routeItem in routeItems) {
+      routeMap[routeItem.route] = routeItem.widget!;
+    }
     return routeMap;
   }
 
   /// 插入路由，检查路由名称是否重复
-  void _insertRoutes(List<AriRouteItem> routes) {
-    routes.forEach((route) {
-      _addRoute(route);
-    });
-  }
+  void _insertRoutes(
+    List<AriRouteItem> routeItems,
+    String parentRoute,
+    AriNavigationBar Function(BuildContext)? navigationBar,
+  ) {
+    for (var routeItem in routeItems) {
+      // 设置route
+      String route = routeItem.route;
+      if (route.startsWith("/")) {
+        route = route.substring(1);
+      }
+      // 设置widegt，如果是底部导航栏，需要设置底部导航栏的配置
+      Widget Function(BuildContext)? widget = routeItem.widget;
+      if (routeItem.hasNavigation) {
+        Widget Function(BuildContext) widget =
+            _generateBottomNavigationContainer(
+          routeItem,
+          navigationBar,
+        );
+      } else {
+        if (widget == null) {
+          assert(false, "当hasNavigation为false时，widget不能为null");
+        }
+      }
 
-  void _addRoute(AriRouteItem route) {
-    final String name = route.name;
-    // final BuildContext context = route.context;
-    if (_isNameUnique(name)) {
-      // assert(false,
-      //     AriLocalizations.of(context)!.ariRouteItem_name_duplicate(name));
-    } else {
-      _routeNames.add(name);
-      _routes.add(route);
-      if (route.children.length > 1) {
-        _insertRoutes(route.children);
+      AriRouteItem newRouteItem = routeItem.copyWith(
+        route: join(parentRoute, route),
+        widget: widget,
+      );
+      _routeItems.add(newRouteItem);
+      if (routeItem.children.length > 1) {
+        _insertRoutes(
+          routeItem.children,
+          newRouteItem.route,
+          navigationBar,
+        );
       }
     }
   }
 
-  bool _isNameUnique(String name) {
-    if (_routeNames.contains(name)) {
-      return true;
+  Widget Function(BuildContext) _generateBottomNavigationContainer(
+    AriRouteItem routeItem,
+    AriNavigationBar Function(BuildContext)? widget,
+  ) {
+    return (context) => _NavigationContainer(
+          routeItem: routeItem,
+          widget: widget,
+        );
+  }
+}
+
+class _NavigationContainer extends StatefulWidget {
+  const _NavigationContainer({
+    super.key,
+    required this.routeItem,
+    required this.navigationBar,
+  });
+
+  /// 导航栏项
+  final AriRouteItem routeItem;
+
+  final Widget Function(BuildContext)? navigationBar;
+
+  @override
+  State<_NavigationContainer> createState() => __NavigationContainerState();
+}
+
+class __NavigationContainerState extends State<_NavigationContainer> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    Widget navigationBar;
+    if (widget.navigationBar != null) {
+      navigationBar = widget.navigationBar!(context);
     } else {
-      return false;
+      navigationBar = AriNavigationBar(
+        itemChangeCallback: (int index, String route) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        navigationItems: widget.routeItem.navigationConfig!.navigationItems,
+      );
     }
+    return Scaffold(
+      /*
+           * 底部导航栏，点击后会改变[selectedIndex]
+           * 从而触发更新setState,重新渲染[RouteWidgets]
+           */
+      bottomNavigationBar: navigationBar,
+      body: AriRouteNavigation(
+          selectedIndex: selectedIndex, routeItem: widget.routeItem),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
