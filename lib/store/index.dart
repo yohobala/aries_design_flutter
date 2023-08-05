786b1b7eea5c824175dca1ec04e9d758264e4990
1997926hh <762100246@qq.com>
2023-08-05 17:34:38 +0800

    新增了状态管理

---
+++ lib/store/index.dart
@@ -0,0 +1,22 @@
+class Store {
+  // 私有构造函数
+  Store._internal();
+  // 单例实例
+  static final Store _instance = Store._internal();
+
+  // 工厂构造函数返回单例实例
+  factory Store() => _instance;
+
+  // 用于存储注册模型的私有映射
+  final Map<Type, dynamic> _models = {};
+
+  /// 注册模型
+  void register<T>(T model) {
+    _models[T] = model;
+  }
+
+  // 作为可调用类，可以直接调用此方法来获取模型
+  T call<T>() {
+    return _models[T] as T;
+  }
+}
