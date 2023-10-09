236cbf0559c227ea05a3eedfde25e50fe4713a0c
1997926hh <762100246@qq.com>
2023-10-09 20:41:07 +0800

    增加堆栈输出

--- lib/exception/exception.dart
+++ lib/exception/exception.dart
@@ -1,14 +1,18 @@
 import 'package:http/http.dart' as http;
 
 class AriException implements Exception {
+  AriException({
+    this.code = -1,
+    this.message = "",
+    this.statckTrace = StackTrace.empty,
+  });
   final String message;
   final int code;
-
-  AriException({this.code = -1, this.message = ""});
+  final StackTrace statckTrace;
 
   @override
   String toString() {
-    return "CustomException: $message (code: $code)";
+    return "CustomException:\nmessage: $message \ncode: $code,\n$statckTrace";
   }
 }
 
@@ -16,9 +20,9 @@ AriException handleAPI(http.Response response) {
   return AriException(code: response.statusCode);
 }
 
-AriException handleException(Object e) {
+AriException handleException(Object e, StackTrace s) {
   if (e is AriException) {
     return e;
   }
-  return AriException(message: e.toString());
+  return AriException(message: e.toString(), statckTrace: s);
 }
