c7cbc8e1b289506be2caabb67910b1f282468e05
1997926hh <762100246@qq.com>
2023-10-30 14:13:40 +0800

    FIXME: 修改了AriNavigationBarScaffold文件夹名字

--- lib/widgets/Index.dart
+++ lib/widgets/Index.dart
@@ -1,4 +1,4 @@
 export 'layout/index.dart';
-export 'navigation_bar_layout/index.dart';
+export 'navigation_bar_scaffold/index.dart';
 export 'gradient_border/index.dart';
 export 'gradient_progress_indicator/index.dart';
